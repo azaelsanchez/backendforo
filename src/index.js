651756c3e0b969a575f22b83a40f7985ef50aa3f
azaelsanchez <cem20619@gmail.com>
2020-03-05 17:00:55 +0100

    routes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ const app = express();
 const morgan = require('morgan');
 
 
+//Configs
+app.set('port',process.env.PORT || 3000)
+
 //middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
+
+//routes
 
 //Inicia el server
-app.listen(3000, () =>{
-    console.log(`Server runnig on ${3000}`);
+app.listen(app.get('port'),() =>{
+    console.log(`Server runnig on ${app.get('port')}`);
 });
